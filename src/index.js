358d3f3f060bc3e086f1c4e5fb4e25c31fa0d5d5
Sergio Gomez <segomez@baufest.com>
2020-03-24 03:41:09 -0300

    video 123

--- src/index.js
+++ src/index.js
@@ -29,14 +29,14 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
+// const main = async () => {
+//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
 
-    const user = await User.findById('5e4f5df2d85b642b5c185bfd')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
+//     const user = await User.findById('5e4f5df2d85b642b5c185bfd')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks)
+// }
 
-main()
+// main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,11 +17,33 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
-        //const tasks = await Task.find({ owner: req.user._id })
-        await req.user.populate('tasks').execPopulate()
-        
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(400).send()
