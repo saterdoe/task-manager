92a863ee3a4bc477636c8999bfb0085d44d6a014
Sergio Gomez <segomez@baufest.com>
2020-03-30 01:59:46 -0300

    video 122

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
-const multer = require('multer')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         const token = await user.generateAuthToken()
@@ -31,9 +31,7 @@ router.post('/users/logout', auth, async (req, res) => {
         req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
         })
-
         await req.user.save()
-
         res.send()
     } catch (e) {
         res.status(500).send()
@@ -43,13 +41,11 @@ router.post('/users/logout', auth, async (req, res) => {
 router.post('/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.tokens = []
-
         await req.user.save()
         res.send()
     } catch (e) {
         res.status(500).send()
     }
-
 })
 
 router.get('/users/me', auth, async (req, res) => {
@@ -66,7 +62,6 @@ router.patch('/users/me', auth, async (req, res) => {
     }
     try {
         updates.forEach((update) => req.user[update] = req.body[update])
-
         await req.user.save()
         res.send(req.user)
     } catch (e) {
@@ -77,7 +72,6 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
     try {
         await req.user.remove()
-
         res.send(req.user)
     } catch (e) {
         return res.status(500).send()
@@ -97,7 +91,8 @@ const upload = multer({
     }
 })
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -113,12 +108,10 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
 router.get('/users/:id/avatar', async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
-
         if (!user || !user.avatar) {
             throw new Error()
         }
-
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch (e) {
         res.status(404).send()
