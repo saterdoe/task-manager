92ba813b14b383d5c7b11aaf2c1f797eceb19cc8
Sergio Gomez <segomez@baufest.com>
2020-04-08 00:51:31 -0300

    video 143

---
+++ src/math.js
@@ -0,0 +1,36 @@
+const calculateTip = (total, tipPercent = .25) => total + (total * tipPercent)
+
+const fahrenheitToCelsius = (temp) => {
+    return (temp - 32) / 1.8
+}
+
+const celsiusToFahrenheit = (temp) => {
+    return (temp * 1.8) + 32
+}
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (a < 0 || b < 0) {
+                return reject('Numbers must be non-negative')
+            }
+
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//
+// Goal: Test temperature conversion functions
+//
+// 1. Export both functions and load them into test suite
+// 2. Create "Should convert 32 F to 0 C"
+// 3. Create "Should convert 0 C to 32 F"
+// 4. Run the Jest to test your work!
+
+module.exports = {
+    calculateTip,
+    fahrenheitToCelsius,
+    celsiusToFahrenheit,
+    add
+}
